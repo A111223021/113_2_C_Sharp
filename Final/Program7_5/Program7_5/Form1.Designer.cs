018ec97bc49836daadcc76b4b80bf50f827d93da
A111223021 <a111223021@mail.shu.edu.tw>
2025-06-05 13:05:59 +0800

    Final

---
+++ Final/Program7_5/Program7_5/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Program7_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Addbutton = new System.Windows.Forms.Button();
+            this.Exitbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 23);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(309, 88);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 80);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "選擇以下其中一個團隊：";
+            // 
+            // Addbutton
+            // 
+            this.Addbutton.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Addbutton.Location = new System.Drawing.Point(27, 221);
+            this.Addbutton.Name = "Addbutton";
+            this.Addbutton.Size = new System.Drawing.Size(122, 32);
+            this.Addbutton.TabIndex = 3;
+            this.Addbutton.Text = "新增資料";
+            this.Addbutton.UseVisualStyleBackColor = true;
+            this.Addbutton.Click += new System.EventHandler(this.Addbutton_Click);
+            // 
+            // Exitbutton
+            // 
+            this.Exitbutton.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Exitbutton.Location = new System.Drawing.Point(192, 221);
+            this.Exitbutton.Name = "Exitbutton";
+            this.Exitbutton.Size = new System.Drawing.Size(116, 32);
+            this.Exitbutton.TabIndex = 4;
+            this.Exitbutton.Text = "離開";
+            this.Exitbutton.UseVisualStyleBackColor = true;
+            this.Exitbutton.Click += new System.EventHandler(this.Exitbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 286);
+            this.Controls.Add(this.Exitbutton);
+            this.Controls.Add(this.Addbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "世界大賽冠軍隊伍";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button Addbutton;
+        private System.Windows.Forms.Button Exitbutton;
+    }
+}
+
---
+++ Final/Program7_5/Program7_5/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Program7_5
+{
+    public struct TeamData
+    {
+        public string Name;
+        public int WinCount;
+        public List<int> WinYears;
+
+        public TeamData(string name)
+        {
+            Name = name;
+            WinCount = 0;
+            WinYears = new List<int>();
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        List<string> teams = new List<string>();
+        List<string> winners = new List<string>();
+        List<int> years = new List<int>();
+        List<string> originalTeams = new List<string>();
+        List<TeamData> teamDataList = new List<TeamData>();
+
+        string originalWinnersPath = "";
+        string teamListPath = "";
+        string newTeamOutputPath = "MLB_Teams_Translated.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("請先選擇球隊資料檔案", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (!ReadTeams()) return;
+
+            MessageBox.Show("再選擇世界大賽冠軍資料檔案", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (!ReadWinners()) return;
+
+            UpdateTeamList();
+            BuildTeamDataList();
+        }
+
+        private bool ReadTeams()
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "請選擇 Teams.txt 檔案";
+                dialog.Filter = "文字檔案 (*.txt)|*.txt";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    teamListPath = dialog.FileName;
+                    teams.Clear();
+                    listBox1.Items.Clear();
+
+                    StreamReader sr = null;
+                    try
+                    {
+                        sr = new StreamReader(dialog.FileName);
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (!teams.Contains(line))
+                                teams.Add(line);
+                        }
+                    }
+                    finally
+                    {
+                        if (sr != null)
+                            sr.Close();
+                    }
+
+                    originalTeams = new List<string>(teams);
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("未選擇檔案，程式即將結束。");
+                    this.Close();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("讀取球隊檔案時發生錯誤：" + ex.Message);
+                return false;
+            }
+        }
+
+        private bool ReadWinners()
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "請選擇 WorldSeries.txt 檔案";
+                dialog.Filter = "文字檔案 (*.txt)|*.txt";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    originalWinnersPath = dialog.FileName;
+                    winners.Clear();
+                    years.Clear();
+
+                    StreamReader sr = null;
+                    try
+                    {
+                        sr = new StreamReader(dialog.FileName);
+                        int year = 1903;
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            if (year == 1904 || year == 1994) year++;
+                            winners.Add(line);
+                            years.Add(year);
+                            year++;
+                        }
+                    }
+                    finally
+                    {
+                        if (sr != null)
+                            sr.Close();
+                    }
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("未選擇檔案，程式結束。");
+                    this.Close();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("讀取冠軍檔案時發生錯誤：" + ex.Message);
+                return false;
+            }
+        }
+
+        private void BuildTeamDataList()
+        {
+            teamDataList.Clear();
+            foreach (string team in teams)
+            {
+                TeamData data = new TeamData(team);
+                for (int i = 0; i < winners.Count; i++)
+                {
+                    if (winners[i] == team)
+                    {
+                        data.WinCount++;
+                        data.WinYears.Add(years[i]);
+                    }
+                }
+                teamDataList.Add(data);
+            }
+        }
+
+        private void UpdateTeamList()
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < teams.Count; i++)
+            {
+                listBox1.Items.Add($"{i + 1}.{teams[i]}隊");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex < 0) return;
+
+            string fullText = listBox1.SelectedItem.ToString();
+            int dotIndex = fullText.IndexOf('.');
+            string teamName = fullText.Substring(dotIndex + 1).Replace("隊", "");
+
+            TeamData data = teamDataList.FirstOrDefault(t => t.Name == teamName);
+
+            if (data.WinCount > 0)
+            {
+                data.WinYears.Sort();
+                string yearList = string.Join(", ", data.WinYears);
+
+                if (data.WinCount == 1)
+                    label1.Text = $"{data.Name}隊於 {data.WinYears[0]} 年獲得 1 次世界冠軍。\n年份：{yearList}";
+                else
+                    label1.Text = $"{data.Name}隊 從 {data.WinYears.First()} 年至 {data.WinYears.Last()} 年，共贏得 {data.WinCount} 次世界冠軍。\n年份：{yearList}";
+            }
+            else
+            {
+                label1.Text = $"{data.Name}隊 尚未贏得世界冠軍。";
+            }
+        }
+
+        private void Addbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "請選擇 2010 年以後的世界大賽資料";
+                dialog.Filter = "文字檔案 (*.txt)|*.txt";
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    List<string> newWinners = new List<string>();
+                    List<string> newTeams = new List<string>();
+
+                    StreamReader sr = null;
+                    try
+                    {
+                        sr = new StreamReader(dialog.FileName);
+                        int startYear = 2010;
+                        int maxYear = years.Count > 0 ? years.Max() : 0;
+                        if (maxYear >= 2010)
+                            startYear = maxYear + 1;
+
+                        string line;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            winners.Add(line);
+                            years.Add(startYear);
+                            newWinners.Add(line);
+
+                            if (!teams.Contains(line))
+                                teams.Add(line);
+
+                            if (!originalTeams.Contains(line) && !newTeams.Contains(line))
+                                newTeams.Add(line);
+
+                            startYear++;
+                        }
+                    }
+                    finally
+                    {
+                        if (sr != null)
+                            sr.Close();
+                    }
+
+                    UpdateTeamList();
+                    BuildTeamDataList();
+
+                    if (newTeams.Count > 0)
+                    {
+                        StreamWriter sw = null;
+                        try
+                        {
+                            sw = new StreamWriter(newTeamOutputPath, true);
+                            foreach (string team in newTeams)
+                            {
+                                sw.WriteLine(team);
+                            }
+                        }
+                        finally
+                        {
+                            if (sw != null)
+                                sw.Close();
+                        }
+
+                        originalTeams.AddRange(newTeams);
+                    }
+
+                    if (!string.IsNullOrEmpty(originalWinnersPath))
+                    {
+                        StreamWriter sw = null;
+                        try
+                        {
+                            sw = new StreamWriter(originalWinnersPath, true);
+                            foreach (string team in newWinners)
+                            {
+                                sw.WriteLine(team);
+                            }
+                        }
+                        finally
+                        {
+                            if (sw != null)
+                                sw.Close();
+                        }
+                    }
+
+                    MessageBox.Show($"新增完成！\n新隊伍數：{newTeams.Count}\n新增冠軍數：{newWinners.Count}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("新增資料時發生錯誤：" + ex.Message);
+            }
+        }
+
+        private void Exitbutton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
+
+
+
+
+
